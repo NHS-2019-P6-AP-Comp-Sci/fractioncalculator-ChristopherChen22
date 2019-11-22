1708badc9c5702b6f339a523291ff4b19fcdf1e8
s-chenchr <s-chenchr@53S500042228.it.bsd405.org>
2019-11-22 13:17:51 -0800

    checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -11,8 +11,12 @@ public class FracCalc {
     	Scanner s = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter your command:");
         String userResponse= s.nextLine(); 
+        while(userResponse.equals("quit")!= true) {
         String answer = produceAnswer(userResponse);
         System.out.println(answer);
+        userResponse = s.nextLine();
+        }
+       
         
 		
 	
@@ -36,13 +40,59 @@ public class FracCalc {
     	String operator = input.substring(space-1);
     	String operand2 = input.substring(space + 3);
     	
-		return operand2;
+		
+	
+		String w = findWhole(operand2);
+		String n = findNum(operand2);
+		String d = findDenom(operand2);
+		
+		String finalAnswer = "whole:" + w + " numerator:" + n + " denominator:" + d;
+		return finalAnswer;
+	
+		
 		
     
         // TODO: Implement this function to produce the solution to the input
  
     }
-
+    public static String findWhole(String str) {
+    	if(str.contains("_")) {
+    		return str.substring(0,str.indexOf('_'));
+    	}
+    	else if(str.contains("/")) {
+    		return "0";
+    	}
+    	else return str;
+    }
+    public static String findNum(String str) {
+    	if(str.contains("_")) {
+    		return str.substring( str.indexOf('_') + 1, str.indexOf('/'));
+    	}
+    	else if(str.contains("/")) {
+    		return str.substring(0, str.indexOf('/'));
+    	}
+    	else {
+    		return "0";
+    	}
+    }
+    public static String findDenom(String str) {
+    	if(str.contains("/")) {
+    		return str.substring(str.indexOf("/") + 1);
+    		
+    	}
+		
+		else {
+			return "1";
+		}
+    	
+		
+	
+    	
+		
+    }
+   
+		
+    }
     // TODO: Fill in the space below with any helper methods that you think you will need
 
-}
+
