680eff3b4b9c293c6a5b3fea6644683fa94777fd
s-chenchr <s-chenchr@53S500042228.it.bsd405.org>
2019-12-02 18:51:46 -0800

    checkpoint 3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -1,5 +1,5 @@
 
-/*** @author Mr. Rasmussen
+/*** @author Mr. Rasmussen;
  */
 
 package fracCalc;
@@ -16,11 +16,6 @@ public class FracCalc {
         System.out.println(answer);
         userResponse = s.nextLine();
         }
-       
-        
-		
-	
-        
         // TODO: Read the input from the user and call produceAnswer with an equation
 
     }
@@ -37,38 +32,68 @@ public class FracCalc {
     public static String produceAnswer(String input) {
     	int space = input.indexOf(" ");
     	String operand1 = input.substring(0, space); 
-    	String operator = input.substring(space-1);
+    	String operator = input.substring(input.indexOf(" ") + 1, input.indexOf(" ") + 2);
     	String operand2 = input.substring(space + 3);
     	
+            
+    	 
+    	String w = findWhole(operand1);
+    	String n= findNum(operand1);
+    	String d = findDenom(operand1);
+    	String w2 = findWhole(operand2);
+		String n2= findNum(operand2);
+		String d2 = findDenom(operand2);
+		int whole = Integer.valueOf(w);
+		int num = Integer.valueOf(n);
+		int denom = Integer.valueOf(d);
+		int whole2 = Integer.valueOf(w2);
+		int num2 = Integer.valueOf(n2);
+		int denom2 = Integer.valueOf(d2); 
+		String answer = "temp";
 		
-	
-		String w = findWhole(operand2);
-		String n = findNum(operand2);
-		String d = findDenom(operand2);
+		 if (operator.equals("+")) {
+			answer = addition(whole, num, denom, whole2, num2, denom2, operand1, operand2);	
+    	 }
+		 else if(operator.equals("-")) {
+			  answer = subtract(whole, num, denom, whole2, num2, denom2);
+			
+		 }
+		 else if(operator.equals("*")) {
+			  answer = multiply(whole,num,denom,whole2,num2,denom2,operand1,operand2);
+			 
+		 }
+		 else if (operator.contentEquals("/")) {
+			 answer = divide(whole,num,denom,whole2,num2,denom2,operand1,operand2);
+			 
+    }
+		 return answer;
+    }
 		
-		String finalAnswer = "whole:" + w + " numerator:" + n + " denominator:" + d;
-		return finalAnswer;
-	
+		 
+    	 
+        // TODO: Implement this function to produce the solution to the input
 		
 		
     
-        // TODO: Implement this function to produce the solution to the input
- 
-    }
     public static String findWhole(String str) {
     	if(str.indexOf("_") == 1) {
-    		return str.substring(0,str.indexOf('_'));
+    		return str.substring(0, str.indexOf('_'));
     	}
     	else if(str.indexOf("/") == 1) {
     		return "0";
     	}
-    	else return str;
+    	else if (str.indexOf("-") != -1) {
+    		return str;
+    	}
+    	else {
+    		return str;
+    	}
     }
     public static String findNum(String str) {
-    	if(str.indexOf("_") != 1) {
+    	if(str.indexOf("_") == 1) {
     		return str.substring( str.indexOf('_') + 1, str.indexOf('/'));
     	}
-    	else if(str.indexOf("/") != 1) {
+    	else if(str.indexOf("/") == 1) {
     		return str.substring(0, str.indexOf('/'));
     	}
     	else {
@@ -76,23 +101,156 @@ public class FracCalc {
     	}
     }
     public static String findDenom(String str) {
-    	if(str.indexOf("/") == 1) {
-    		return str.substring(str.indexOf("/") + 1);
-    		
-    	}
-		
+    	if(str.indexOf("/") != 0) {
+    		return str.substring(str.indexOf("/") + 1);	
+    	}		
 		else {
 			return "1";
 		}
+		}
+    
+    public static String addition(int w1, int n1, int d1, int w2, int n2, int d2, String first, String second) {	 
+    	int numerator1 = n1 * d2;
+        int numerator2 = n2 * d1;
+        int denominator = d1 * d2;
+        int whole = w1 + w2;
+        
+        if (first.indexOf("-") != -1 && second.indexOf("-") != -1) {
+                numerator1 *= -1;
+                numerator2 *= -1;
+        } else if (second.indexOf("-") != -1 && first.indexOf("-") == -1) {
+                numerator2 *= -1;
+        } else if (second.indexOf("-") == -1 && first.indexOf("-") != -1) {
+                numerator1 *= -1;
+        }
+     
+        int numerator = numerator1 + numerator2;
+        
+        if (numerator / denominator >= 1) {
+                int addWhole = numerator / denominator;
+                numerator = Math.abs((addWhole * denominator) - numerator);
+                whole += addWhole;
+        }
+       
+        String total = whole + "_" + Math.abs(numerator) + "/" + denominator;
+ 
+        double testNum = numerator;
+        double testDem = denominator;
+        if ((testNum / testDem) == 0.0) {
+                total = whole + "";
+               
+        } else if (whole == 0) {
+                total = numerator + "/" + denominator;
+        }
+       
+        return total;
+ }
+
     	
-		
-	
-    	
-		
-    }
-   
-		
-    }
+    
+        public static String subtract(int whole1, int nom1, int denom1, int whole2, int nom2, int denom2) {
+            
+            int denominator = denom1 * denom2;
+            
+            int numerator1 = nom1 * denom2;
+            int numerator2 = nom2 * denom1;
+            int numerator = numerator1 - numerator2;
+            
+            int whole = whole1 - whole2;
+           
+            if (numerator / denominator >= 1) {
+                    int addWhole = numerator / denominator;
+                    numerator = Math.abs((addWhole * denominator) - numerator);
+                    whole += addWhole;
+            }
+           
+            String total = whole + "_" + Math.abs(numerator) + "/" + denominator;
+           
+            double testNum = numerator;
+            double testDem = denominator;
+            if ((testNum / testDem) == 0.0) {
+                    total = whole + "";
+                    
+            } else if (whole == 0) {
+                    total = numerator + "/" + denominator;
+            }
+       
+            return total;
+     }
+        public static String multiply(int whole1, int nom1, int denom1, int whole2, int nom2, int denom2, String first, String second) {
+        	 int numerator1 = whole1 * denom1;
+             int numerator2 = whole2 * denom2;
+            int denominator = denom1 * denom2;
+            
+           
+            if (first.indexOf("-") != -1 && second.indexOf("-") != -1) {
+                    nom1 *= -1;
+                    nom2 *= -1;
+            } else if (second.indexOf("-") != -1 && first.indexOf("-") == -1) {
+                    nom2 *= -1;
+            }
+            numerator1 += nom1; 
+            numerator2 += nom2;
+            int numerator = numerator1 * numerator2;
+            int whole = 0;
+            if (numerator / denominator >= 1) {
+                    int addWhole = numerator / denominator; 
+                    numerator = Math.abs((addWhole * denominator) - numerator); 
+                    whole += addWhole; 
+            }
+            String total = whole + "_" + numerator + "/" + denominator;
+            double testNum = numerator;
+            double testDem = denominator;
+            if ((testNum / testDem) == 0.0) {
+                    total = whole + "";
+                    // test to see if there is no whole number (make total just a fraction)
+            } else if (whole == 0) {
+                    total = numerator + "/" + denominator;
+            }
+            // total =
+            // simplifier(total);*****************************************************
+            return total;
+     }
+
+        public static String divide(int whole1, int nom1, int denom1, int whole2, int nom2, int denom2, String first, String second) {
+            // multiply (find) denominator
+            int denominator = denom1;
+            // find individual numerators
+            int numerator1 = whole1 * denom1;
+            int numerator2 = whole2 * denom2;
+            if (first.indexOf("-") != -1 && second.indexOf("-") != -1) {
+                    nom1 *= -1;
+                    nom2 *= -1;
+            } else if (second.indexOf("-") != -1 && first.indexOf("-") == -1 && second.indexOf("_") != -1) {
+                    nom2 *= -1;
+            }
+           
+            numerator1 += nom1;
+            numerator2 += nom2;
+            int numerator = numerator1 * denom2;
+            denominator *= numerator2;
+            int whole = 0;
+            if (numerator / denominator >= 1) {
+                    int addWhole = numerator / denominator; // 12
+                    numerator = Math.abs((addWhole * denominator) - numerator); // 0
+                    whole += addWhole; // 12
+                    denominator = Math.abs(denominator);
+            }
+           
+            String total = whole + "_" + numerator + "/" + denominator;
+            double testNum = numerator;
+            double testDem = denominator;
+            if ((testNum / testDem) == 0.0) {
+                    total = whole + "";
+                    
+            } else if (whole == 0) {
+                    total = numerator + "/" + denominator;
+            }
+          
+            return total;
+
+        }
+}
     // TODO: Fill in the space below with any helper methods that you think you will need
 
 
